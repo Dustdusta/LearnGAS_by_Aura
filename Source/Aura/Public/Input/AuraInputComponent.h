c159bd46c499223a1b5029ec363daa3d79fc54d6
Due <Due@co.com>
2024-06-03 09:37:52 +0800

    新增AuraInputComponent，实现EnhancedInput的输入绑定通用函数

---
+++ Source/Aura/Public/Input/AuraInputComponent.h
@@ -0,0 +1,52 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AuraInputConfig.h"
+#include "EnhancedInputComponent.h"
+#include "AuraInputComponent.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AURA_API UAuraInputComponent : public UEnhancedInputComponent
+{
+	GENERATED_BODY()
+
+public:
+	// PressedFuncType是按下输入时绑定的函数类型
+	template <class UserClass, typename PressedFuncType, typename ReleasedFuncType, typename HeldFuncType>
+	void BindAbilityAction(const UAuraInputConfig* InputConfig, UserClass Object, PressedFuncType PressedFunc, ReleasedFuncType ReleasedFunc, HeldFuncType HeldFunc);
+};
+
+template <class UserClass, typename PressedFuncType, typename ReleasedFuncType, typename HeldFuncType>
+void UAuraInputComponent::BindAbilityAction(const UAuraInputConfig* InputConfig, UserClass Object, PressedFuncType PressedFunc, ReleasedFuncType ReleasedFunc, HeldFuncType HeldFunc)
+{
+	check(InputConfig);
+
+	for (const FAuraInputAction& Action : InputConfig->AbilityInputActions)
+	{
+		if (Action.InputAction && Action.InputTag.IsValid())
+		{
+			if(PressedFunc)
+			{
+				// 按键一旦按下，我们就会绑定这个函数回调，一旦启动，只会被调用一次
+				BindAction(Action.InputAction,ETriggerEvent::Started, Object, PressedFunc, Action.InputTag);
+			}
+			
+			if(ReleasedFunc)
+			{
+				// 停止输入时，我们会调用这个函数回调
+				BindAction(Action.InputAction,ETriggerEvent::Completed, Object, PressedFunc, Action.InputTag);
+			}
+			
+			if (HeldFunc)
+			{
+				// 希望每帧都调用此函数回调，则选择"Triggered"。传入Action.InputTag：与给定操作关联的任何输入标记都将传递到其回调中
+				BindAction(Action.InputAction, ETriggerEvent::Triggered, Object, HeldFunc, Action.InputTag);
+			}
+		}
+	}
+}
