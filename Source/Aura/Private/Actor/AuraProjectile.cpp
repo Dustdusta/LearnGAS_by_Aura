4ac7465bdb8cb201d740118b6f3b6c8910b08b8c
Dustdusta <dustdusta@outlook.com>
2024-10-23 15:28:07 +0800

    火球新增碰撞与对应音效，在和其余Actor重叠时销毁。

--- Source/Aura/Private/Actor/AuraProjectile.cpp
+++ Source/Aura/Private/Actor/AuraProjectile.cpp
@@ -3,15 +3,18 @@
 
 #include "Actor/AuraProjectile.h"
 
+#include "NiagaraFunctionLibrary.h"
+#include "Components/AudioComponent.h"
 #include "Components/SphereComponent.h"
 #include "GameFramework/ProjectileMovementComponent.h"
+#include "Kismet/GameplayStatics.h"
 
 AAuraProjectile::AAuraProjectile()
 {
 	PrimaryActorTick.bCanEverTick = true;
 	// 设置是否允许客户端复制
 	bReplicates = true;
-	
+
 	// 生成并设置球体碰撞体组件的相关信息
 	Sphere = CreateDefaultSubobject<USphereComponent>("Sphere");
 	SetRootComponent(Sphere);
@@ -31,10 +34,48 @@ AAuraProjectile::AAuraProjectile()
 void AAuraProjectile::BeginPlay()
 {
 	Super::BeginPlay();
+	// 设置投射物的生命期限，在此期限后投射物将自动销毁
+	SetLifeSpan(LifeSpan);
+	// 给投射物的碰撞组件（Sphere）注册一个开始重叠的事件回调，当与其他物体发生重叠时会触发
 	Sphere->OnComponentBeginOverlap.AddDynamic(this, &AAuraProjectile::OnSphereOverlap);
+	// 创建并绑定一个循环播放的声音组件到投射物的根组件上
+	LoopingSoundComponent = UGameplayStatics::SpawnSoundAttached(LoopingSound, GetRootComponent());
+}
+
+void AAuraProjectile::Destroyed()
+{
+	// 如果投射物没有命中目标，并且不是服务器端（即没有权威）
+	if (!bHit && !HasAuthority())
+	{
+		// 在投射物的位置播放撞击音效
+		UGameplayStatics::PlaySoundAtLocation(this, ImpactSound, GetActorLocation(), FRotator::ZeroRotator);
+		// 在投射物的位置生成粒子效果
+		UNiagaraFunctionLibrary::SpawnSystemAtLocation(this, ImpactEffect, GetActorLocation());
+		// 停止循环音效的播放
+		LoopingSoundComponent->Stop();
+	}
+	// 调用父类的Destroyed方法，完成销毁过程
+	Super::Destroyed();
 }
 
 void AAuraProjectile::OnSphereOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
 {
+	// 播放撞击音效
+	UGameplayStatics::PlaySoundAtLocation(this, ImpactSound, GetActorLocation(), FRotator::ZeroRotator);
+	// 生成粒子效果
+	UNiagaraFunctionLibrary::SpawnSystemAtLocation(this, ImpactEffect, GetActorLocation());
+	// 停止循环音效的播放
+	LoopingSoundComponent->Stop();
 	
+	// 如果是服务器端（有权威）
+	if (HasAuthority())
+	{
+		// 销毁投射物
+		Destroy();
+	}
+	else
+	{
+		// 标记投射物为已命中
+		bHit = true;
+	}
 }
