c6b173945983f75e80ca88a36e68b73b3f6a5012
Dustdusta <dustdusta@outlook.com>
2024-12-20 18:26:08 +0800

    继承FGameplayEffectContext以新增我们需要的一些参数，方便数据的传输

---
+++ Source/Aura/Public/AuraAbilityTypes.h
@@ -0,0 +1,40 @@
+#pragma once
+
+#include "GameplayEffectTypes.h"
+
+#include "AuraAbilityTypes.generated.h"
+
+/**
+ * 继承FGameplayEffectContext以新增我们需要的一些参数，方便数据的传输
+ */
+USTRUCT(BlueprintType)
+struct FAuraGameplayEffectContext : public FGameplayEffectContext
+{
+	GENERATED_BODY()
+
+public:
+	// 新增参数的获取
+	bool IsCriticalHit() const { return bIsCriticalHit; }
+	bool IsBlockedHit() const { return bIsBlockedHit; }
+
+	void SetIsCriticalHit(bool bInIsCriticalHit) { bIsCriticalHit = bInIsCriticalHit; }
+	void SetIsBlockedHit(bool bInIsBlockedHit) { bIsBlockedHit = bInIsBlockedHit; }
+
+	/** Returns the actual struct used for serialization, subclasses must override this! */
+	virtual UScriptStruct* GetScriptStruct() const
+	{
+		return FGameplayEffectContext::StaticStruct();
+	}
+
+	/** Custom serialization, subclasses must override this */
+	virtual bool NetSerialize(FArchive& Ar, class UPackageMap* Map, bool& bOutSuccess);
+
+protected:
+	
+	// 新增的参数
+	UPROPERTY()
+	bool bIsBlockedHit = false;
+
+	UPROPERTY()
+	bool bIsCriticalHit = false;
+};
