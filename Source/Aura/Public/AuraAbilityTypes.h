6b94bc07f564496ddfcb90c41dc0b07450681d28
Dustdusta <dustdusta@outlook.com>
2024-12-23 10:36:31 +0800

    复写FAuraGameplayEffectContext::NetSerialize()以实现添加自定义的数据到传输列表

--- Source/Aura/Public/AuraAbilityTypes.h
+++ Source/Aura/Public/AuraAbilityTypes.h
@@ -26,6 +26,7 @@ public:
 		return FGameplayEffectContext::StaticStruct();
 	}
 
+	// 复写NetSerialize()以实现添加自定义的数据到传输列表
 	/** Custom serialization, subclasses must override this */
 	virtual bool NetSerialize(FArchive& Ar, class UPackageMap* Map, bool& bOutSuccess);
 
