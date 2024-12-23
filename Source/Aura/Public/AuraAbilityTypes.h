ac38d2820287ffecea4d0b0d729f7c9a6157e81f
Dustdusta <dustdusta@outlook.com>
2024-12-23 10:57:25 +0800

    创建FAuraGameplayEffectContext的TStructOpsTypeTraits模板

--- Source/Aura/Public/AuraAbilityTypes.h
+++ Source/Aura/Public/AuraAbilityTypes.h
@@ -23,7 +23,24 @@ public:
 	/** Returns the actual struct used for serialization, subclasses must override this! */
 	virtual UScriptStruct* GetScriptStruct() const
 	{
-		return FGameplayEffectContext::StaticStruct();
+		// UE5.3.2特需修改：此处返回无需限定
+		// return FGameplayEffectContext::StaticStruct();
+		return StaticStruct();
+
+	}
+
+	/** Creates a copy of this context, used to duplicate for later modifications */
+	// UE5.3.2特需修改：Duplicate()需返回自定义的类，此处为FAuraGameplayEffectContext
+	virtual FAuraGameplayEffectContext* Duplicate() const
+	{
+		FAuraGameplayEffectContext* NewContext = new FAuraGameplayEffectContext();
+		*NewContext = *this;
+		if (GetHitResult())
+		{
+			// Does a deep copy of the hit result
+			NewContext->AddHitResult(*GetHitResult(), true);
+		}
+		return NewContext;
 	}
 
 	// 复写NetSerialize()以实现添加自定义的数据到传输列表
@@ -38,4 +55,15 @@ protected:
 
 	UPROPERTY()
 	bool bIsCriticalHit = false;
+};
+
+// 创建FAuraGameplayEffectContext的TStructOpsTypeTraits模板
+template<>
+struct TStructOpsTypeTraits<FAuraGameplayEffectContext> : public TStructOpsTypeTraitsBase2<FAuraGameplayEffectContext>
+{
+	enum
+	{
+		WithNetSerializer = true,
+		WithCopy = true,
+	};
 };
